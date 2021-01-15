529c0dadf90b42f4a24b2023f3c98ae0e37e5f41
fuzusheng <fuzusheng@gmail.com>
2021-01-15 09:46:47 +0800

    sink es

--- src/main/java/com/fuzs/App.java
+++ src/main/java/com/fuzs/App.java
@@ -1,16 +1,21 @@
 package com.fuzs;
 
-
 import com.fuzs.model.MetricEvent;
-import com.fuzs.schemas.MetricEventSchema;
+import com.fuzs.utils.ESSinkUtil;
 import com.fuzs.utils.ExecutionEnvUtil;
+import com.fuzs.utils.GsonUtil;
 import com.fuzs.utils.KafkaConfigUtil;
 
-import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer011;
+import org.apache.flink.streaming.connectors.elasticsearch.RequestIndexer;
+import org.apache.flink.api.common.functions.RuntimeContext;
+import static com.fuzs.constant.PropertiesConstant.*;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.Requests;
+import org.elasticsearch.common.xcontent.XContentType;
+import java.util.List;
 
 
 /**
@@ -23,11 +28,17 @@ public class App {
         StreamExecutionEnvironment env = ExecutionEnvUtil.prepare(parameterTool);
         DataStreamSource<MetricEvent> data = KafkaConfigUtil.buildSource(env);
         data.print();
-        // data.addSink(new FlinkKafkaProducer011<MetricEvent>(
-        //         "10.10.14.48:9092",
-        //         "test",
-        //         new MetricEventSchema()
-        // ));
+        List<HttpHost> esAddresses = ESSinkUtil.getEsAddresses(parameterTool.get(ELASTICSEARCH_HOSTS));
+        int bulkSize = parameterTool.getInt(ELASTICSEARCH_BULK_FLUSH_MAX_ACTIONS,40);
+        int sinkParallelism = parameterTool.getInt(STREAM_SINK_PARALLELISM,1);
+
+        ESSinkUtil.addSink(esAddresses, bulkSize, sinkParallelism, data,
+        (MetricEvent metric, RuntimeContext runtimeContext, RequestIndexer requestIndexer) -> {
+            requestIndexer.add(Requests.indexRequest()
+                    .index(FUZS + "_" + metric.getName())
+                    .source(GsonUtil.toJSONBytes(metric), XContentType.JSON));
+        },
+        parameterTool);
         env.execute();
         
     }
--- src/main/java/com/fuzs/constant/PropertiesConstant.java
+++ src/main/java/com/fuzs/constant/PropertiesConstant.java
@@ -16,4 +16,10 @@ public class PropertiesConstant {
     public static final String STREAM_PARALLELISM = "stream.parallelism";
     public static final String STREAM_CHECKPOINT_ENABLE = "stream.checkpoint.enable";
     public static final String STREAM_CHECKPOINT_INTERVAL = "stream.checkpoint.interval";
+    public static final String STREAM_SINK_PARALLELISM = "stream.sink.parallelism";
+
+    public static final String ELASTICSEARCH_BULK_FLUSH_MAX_ACTIONS = "elasticsearch.bulk.flush.max.actions";
+    public static final String ELASTICSEARCH_HOSTS = "elasticsearch.hosts";
+
+    public static final String FUZS = "fuzusheng";
 }
